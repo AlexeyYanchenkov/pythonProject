33343d1af9e2c132d7689ee950a681912ff7dca4
AlexeyYanchenkov <alexeyyanchenkov25@gmail.com>
2024-03-02 13:57:38 +0300

    2355326641

--- funcs.py
+++ funcs.py
@@ -2,9 +2,9 @@ def addition_funcs():
     a = input()
     b = input()
     c = a + b
-    print(f"Ope123 {c}")
+    print(f"Ope1 {c}")
 def subtraction_funcs():
         d = input()
         f = input()
         g = d - f
-        print(f"open 213124 {g}")
+        print(f"open 123 {g}")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from funcs import addition_funcs
 from funcs import subtraction_funcs
 
-
+#vizov
 subtraction_funcs()
 addition_funcs()
 
